c1a84cbd97442c5ea044210375bf8076a427196c
Brandon van Niekerk <vnkbrand@gmail.com>
2018-11-22 17:36:22 +0200

    2nd Commit
    2nd Commit

--- js/script.js
+++ js/script.js
@@ -18,4 +18,69 @@ $(document).ready(function() {
     startDelay: 500
   });
 
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 4,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        938:{
+            items:4
+        },
+        1118:{
+            items:5
+        }
+    }
+});
+
+
+  //Action of Piechart when viewing it
+  var skillsTopOffset = $(".skillsSection").offset().top;
+  var statsTopOffset = $(".statsSection").offset().top;
+  var countUpFinished = false;
+
+  
+  $(window).scroll(function() {
+
+    if(window.pageYOffset > skillsTopOffset - $(window).height() + 300) {
+
+      $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 6,
+        size: 152,
+        animate: ({duration: 2000, enabled: true}),
+        onStep: function(from, to, percent) {
+          $(this.el).find('.percent').text(Math.round(percent));
+        }
+
+    });
+
+    }
+
+
+    if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 300) {
+
+      $(".counter").each(function() {
+        var element = $(this);
+        var endVal = parseInt(element.text());
+  
+        element.countup(endVal);
+      })
+
+      countUpFinished = true;
+  
+    }
+
+  });
+
 });
