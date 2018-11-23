cd77a452add0b027c850263f91bdb0bdd9edb375
Brandon van Niekerk <vnkbrand@gmail.com>
2018-11-23 10:42:42 +0200

    Final before Portfolio
    Final before Portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+//On Load Function
+
+$(window).on("load", function() {
+  $(".loader .inner").fadeOut(500, function() {
+    $(".loader").fadeOut(750);
+  });
+
+  $(".items").isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 1500,
+      easing: 'linear',
+      queue: false
+    }
+  });
+});
+
 //This code will execute when the document is ready
 $(document).ready(function() {
 
@@ -83,4 +100,53 @@ $(document).ready(function() {
 
   });
 
+    $("[data-fancybox]").fancybox();
+
+    $("#filters a").click(function() {
+
+      $("#filters .current").removeClass("current");
+      $(this).addClass("current");
+
+      var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+          filter: selector,
+          animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+          }
+        });
+
+        return false;
+
+    });
+
+    // Scroll Transition to Section
+    $("#navigation li a").click(function(e) {
+      e.preventDefault();
+
+      var targetElement = $(this).attr("href");
+      var targetPosition = $(targetElement).offset().top;
+      $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow")
+    });
+
+    // Add class to NavBar when at top of browser to fix it
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+      function stickyNavigation() {
+
+        var body = $("body");
+        
+        if($(window).scrollTop() >= navTop) {
+          body.css("padding-top", nav.outerHeight() + "px");
+          body.addClass("fixedNav");
+        } else {
+          body.css("padding-top", 0);
+          body.removeClass("fixedNav");
+        }
+      }
 });
